a11241ef7fb3dbd43b14e54512ee5b122a918928
chriji87LU <chriji1987@gmail.com>
2025-12-11 16:11:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'hundegeschirr-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+  // Wenn du eine eigene CSS-/JS-Datei hast, hier ergänzen
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Wenn im Cache, nimm Cache, sonst normal aus dem Netz
+      return response || fetch(event.request);
+    })
+  );
+});
